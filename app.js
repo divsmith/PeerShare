5dd10093de92b15cf0382fa1b8982e7a6e459551
Parker Smith <parker@parkersmith.us>
2018-08-22 20:27:49 -0600

    Successfully connect 2 peers and send text between

--- app.js
+++ app.js
@@ -8,5 +8,28 @@ var peer;
     peer.on('open', function(id) {
         console.log('id: ' + id);
     });
+
+    peer.on('connection', function(conn) {
+        conn.on('open', function() {
+            conn.send('Hello world!');
+        })
+    });
+
     console.log('working');
+
+    server = confirm("Server?");
+
+    if (!server) {
+        dest_id = prompt("ID?");
+
+        var conn = peer.connect(dest_id);
+
+        conn.on('open', function() {
+            console.log('client conn');
+            conn.on('data', function(data) {
+                console.log('client data');
+                console.log(data);
+            });
+        });
+    }
 })();
